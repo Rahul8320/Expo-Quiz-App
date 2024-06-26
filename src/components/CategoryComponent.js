1e27598c38950cf76bf4364d507eca8c7485a8e7
Rahul Dey <rahuldey832000@gmail.com>
2024-06-26 12:38:36 +0530

    Fixed: score reset issue

--- src/components/CategoryComponent.js
+++ src/components/CategoryComponent.js
@@ -1,12 +1,19 @@
+import { useContext } from "react";
 import { TouchableOpacity, Text } from "react-native";
 import tw from "twrnc";
+import { AppContext } from "../context/AppContext";
 
 const CategoryComponent = ({ category, navigation }) => {
+  const { setScore } = useContext(AppContext);
+
+  const handleSubmit = () => {
+    setScore(0);
+    navigation.navigate("Question", { categoryId: category.id });
+  };
+
   return (
     <TouchableOpacity
-      onPress={() =>
-        navigation.navigate("Question", { categoryId: category.id })
-      }
+      onPress={handleSubmit}
       style={tw`bg-slate-600 px-3 m-3 w-42 h-20 items-center justify-center rounded-full`}
     >
       <Text style={tw`font-medium text-lg text-white`}>{category.name}</Text>
